0897c3dba4ce021a11ff050e31110126b618e3fe
SteevesSaillant <steevessaillant@gmail.com>
2022-08-26 14:39:07 -0400

    merge steps  Run Cypress tests and attach Cypress screenshots to Cucumber JSON report

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ rm -f cypress/cucumber-json/*
 npm run test
 npm run attach_screenshots
 node_modules/cucumber-json-merge/bin/cucumber-json-merge  -d cypress/cucumber-json/
-npm attach_screenshots
+npm run attach_screenshots
