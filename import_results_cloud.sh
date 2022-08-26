0d834c2c3224db64d26b678706a11aa1a9c8a618
SteevesSaillant <steevessaillant@gmail.com>
2022-08-26 15:27:48 -0400

    add hard-coded exec key for now

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -2,4 +2,4 @@
 #test push for github action
 BASE_URL=https://xray.cloud.getxray.app
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
-curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token"  --data @"merged-test-results.json" "$BASE_URL/api/v1/import/execution/cucumber"
+curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token"  --data @"merged-test-results.json" "$BASE_URL/api/v1/import/execution/cucumber?keys=AQADX-8"
