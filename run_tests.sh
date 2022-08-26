419bb4c99b1c302818c6ac3153b5f181a3bd189f
SteevesSaillant <steevessaillant@gmail.com>
2022-08-26 13:28:02 -0400

    call run_tests.sh directly form yaml

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 rm -f cypress/cucumber-json/*
-# npm run test:local
 npm run test
 npm run attach_screenshots
 node_modules/cucumber-json-merge/bin/cucumber-json-merge  -d cypress/cucumber-json/
