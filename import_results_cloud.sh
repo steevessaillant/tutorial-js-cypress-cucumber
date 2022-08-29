160148f117790b91b1126d2b8e939d7bff0c898a
SteevesSaillant <steevessaillant@gmail.com>
2022-08-29 10:26:12 -0400

    keep .old file for temp reference

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #test push for github action
 BASE_URL=https://xray.cloud.getxray.app
-token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
+token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json --info @testexec_could_template" "$BASE_URL/api/v1/authenticate"| tr -d '"')
 curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token"  --data @"merged-test-results.json" "$BASE_URL/api/v1/import/execution/cucumber"
