465bc433ee920ae78cdcefe0eff81c44b8ba982e
SteevesSaillant <steevessaillant@gmail.com>
2022-08-29 16:45:39 -0400

    no keys here the testExecKey is found in the export_features_cloud.sh file

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -2,4 +2,4 @@
 #test push for github action
 BASE_URL=https://xray.cloud.getxray.app
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
-curl -H "Content-Type: application/json" -H "Authorization: Bearer $token" "$BASE_URL/api/v2/import/execution/cucumber?testExecKey=AQADX-40" --data @"merged-test-results.json"
+curl -H "Content-Type: application/json" -H "Authorization: Bearer $token" "$BASE_URL/api/v2/import/execution/cucumber" --data @"merged-test-results.json"
