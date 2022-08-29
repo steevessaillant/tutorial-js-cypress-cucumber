ebf1e153a18128a585144d939666a455153f3dd6
SteevesSaillant <steevessaillant@gmail.com>
2022-08-29 11:11:15 -0400

    try one last time with multipart form post

--- import_results_cloud.sh
+++ import_results_cloud.sh
@@ -2,4 +2,4 @@
 #test push for github action
 BASE_URL=https://xray.cloud.getxray.app
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" "$BASE_URL/api/v1/authenticate"| tr -d '"')
-curl -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token" --info @"testexec.json" --data @"merged-test-results.json" "$BASE_URL/api/v1/import/execution/cucumber"
+curl -F info=@"testexec.json" -F data="merged-test-results.json" -H "Content-Type: application/json" -X POST -H "Authorization: Bearer $token" "$BASE_URL/api/v1/import/execution/cucumber/multipart"
