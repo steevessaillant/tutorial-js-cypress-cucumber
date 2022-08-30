2a2d0d1418e807ccc10a58b0b6d2d1b0e17b1f8d
SteevesSaillant <steevessaillant@gmail.com>
2022-08-30 15:57:32 -0400

    fix shell condition syntax

--- logic.sh
+++ logic.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 KEYS="NULL"
-if($BROWSER== "chrome")
+if [[ $BROWSER -eq "chrome" ]]
     then
         KEYS="AQADX-46"  # use testExecKeys not directly test keys
-elif($BROWSER=="firefox")
+elif [[ $BROWSER -eq "firefox" ]]
     then    
         KEYS="AQADX-45"  # use testExecKeys not directly test keys
 else
     KEYS="AQADX-30"  # use testExecKeys not directly test keys
 fi
 
-echo $KEYS
+echo "Selected browser is: " $KEYS
