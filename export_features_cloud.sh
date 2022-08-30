f41811bf36b69414ebc0d6d94dde411c54484b62
SteevesSaillant <steevessaillant@gmail.com>
2022-08-30 16:11:24 -0400

    logic for selecting environments based on browser should work this time

--- export_features_cloud.sh
+++ export_features_cloud.sh
@@ -3,15 +3,17 @@
 BASE_URL=https://xray.cloud.getxray.app
 # here KEYS will contain Test Execution Keys that contains the test ,if you specify the test keys instead a new execution will created and WE DO NOT WANT THIS
 # in order to reuse a test execution we must asscociate the test issue to the specifed executions 
-if( ${{ env.BROWSER }} == "chrome")
+KEYS="NULL"
+if [[ $BROWSER == "chrome" ]]
     then
         KEYS="AQADX-46"  # use testExecKeys not directly test keys
-elif(${{ env.BROWSER }} == "firefox")
+elif [[ $BROWSER == "firefox" ]]
     then    
         KEYS="AQADX-45"  # use testExecKeys not directly test keys
 else
     KEYS="AQADX-30"  # use testExecKeys not directly test keys
 fi
+
 token=$(curl -H "Content-Type: application/json" -X POST --data @"cloud_auth.json" $BASE_URL/api/v1/authenticate| tr -d '"')
 curl -H "Content-Type: application/json" -X GET -H "Authorization: Bearer $token" "$BASE_URL/api/v1/export/cucumber?keys=$KEYS" -o features.zip
 
