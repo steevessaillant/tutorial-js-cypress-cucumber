3f038e3de544f658c17f8f8b404b20470b996d01
SteevesSaillant <steevessaillant@gmail.com>
2022-08-30 12:38:16 -0400

    specify verison and devDeps

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -f cypress/cucumber-json/*
-npm install
+npm install cypress@7.5.0 -D
 npm run test
 npm run attach_screenshots
 node_modules/cucumber-json-merge/bin/cucumber-json-merge  -d cypress/cucumber-json/
