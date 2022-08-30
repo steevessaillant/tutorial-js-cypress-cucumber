c4d752cbb9f4bc6205c48300978863a03e7a508f
SteevesSaillant <steevessaillant@gmail.com>
2022-08-30 12:55:36 -0400

    try run cypress with github action cypress-io/github-action@v4

---
+++ run_cleanup.sh
@@ -0,0 +1,1 @@
+rm -f cypress/cucumber-json/*
--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-rm -f cypress/cucumber-json/*
-npm install cypress@7.5.0 -D --legacy-peer-deps
-npm run test
 npm run attach_screenshots
 node_modules/cucumber-json-merge/bin/cucumber-json-merge  -d cypress/cucumber-json/
 npm run attach_screenshots
