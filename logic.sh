3cf6dc91b1b40956bb07c234995895923a9b8b4f
SteevesSaillant <steevessaillant@gmail.com>
2022-08-30 15:25:42 -0400

    try print form sh file

---
+++ logic.sh
@@ -0,0 +1,12 @@
+KEYS = "NULL"
+if( ${{ env.BROWSER }} == "chrome")
+    then
+        KEYS="AQADX-46"  # use testExecKeys not directly test keys
+elif(${{ env.BROWSER }} == "firefox")
+    then    
+        KEYS="AQADX-45"  # use testExecKeys not directly test keys
+else
+    KEYS="AQADX-30"  # use testExecKeys not directly test keys
+fi
+
+echo $KEYS
